c5503f557ed5a73b5c97883785c3aee9db5fb8f2
Filip Pavetic <fpavetic@gmail.com>
2013-06-05 22:12:57 +0200

    Organizirani mapper.

--- src/core/mapping.cpp
+++ src/core/mapping.cpp
@@ -223,13 +223,112 @@ void populatePositions(map<int, shared_ptr<vector<pair<int, int> > > >& position
   }
 }
 
-void windowedAlignment() {
-  if (FLAGS_long_read_algorithm == "lis") {
+// TODO: ova se funkcija moze srediti tako da koristi 
+// helper funkcije napisane u ovom fileu,
+// trenutno je ovo samo kopi pejst nekog starog commita
+// PROBLEM: cini se da ne radi trenutno bas
+void windowedAlignment(shared_ptr<Read> read,
+		       map<int, shared_ptr<vector<pair<int, int> > > >& positionsByGene,
+		       const shared_ptr<Index> idx,
+		       const vector<shared_ptr<Gene> >& genes,
+		       const int rc) {
+  // a) prvo procijenim koje su pozicije najizglednije
+  // da na njih smjestam read
+  int seedLen = idx->getSeedLen();
+
+  map<pair<int, int>, int> beginEstimate;
+  int totalCount = 0;
+
+  for (auto candidateGenes : positionsByGene) {
+    int geneIdx = candidateGenes.first;
     
-  } else if (FLAGS_long_read_algorithm == "coverage") {
+    vector<pair<int, int> >& positions = *candidateGenes.second;
+    for (size_t i = 0; i < positions.size(); ++i) {
+      int position = positions[i].first;
+      int kmer = positions[i].second;
+      const int blockSize = 20;
+      int block = (position-kmer)/blockSize;
+      ++beginEstimate[make_pair(geneIdx, max(0,block*blockSize))];
+      ++totalCount;
+    }
+  }
+  
+  // b) odredim prozore za provjeru, a to su oni koji imaju zajedno
+  // preko odredjenog postotka hitova
+  vector<pair<int, pair<int, int> > > revBegEst;
+  for (auto iter : beginEstimate) {
+    revBegEst.push_back(make_pair(iter.second, iter.first));
+  }
+  sort(revBegEst.rbegin(), revBegEst.rend());
+  
+  map<int, shared_ptr<vector<int> > > candidatePositionsByGene;
+  for (size_t i = 0; i < revBegEst.size(); ++i) {
+    int geneIdx = revBegEst[i].second.first;
+    int pos = revBegEst[i].second.second;
+    shared_ptr<vector<int> >& vecPtr =
+      candidatePositionsByGene[geneIdx];
+    if (!vecPtr) {
+      vecPtr = shared_ptr<vector<int> > (new vector<int>());
+    }
+    vecPtr->push_back(pos);
+  }
+  
+  for (auto candidatePositions : candidatePositionsByGene) {
+    int geneIdx = candidatePositions.first;
+    vector<int>& starts = *candidatePositions.second;
+    sort(starts.begin(), starts.end());
     
-  } else {
-    printUsageAndExit();
+    vector<pair<int, int> >& positions = *positionsByGene[geneIdx];
+    sort(positions.begin(), positions.end());
+    
+    int windowSize = 2*read->size();
+    int b = 0, e = 0;
+    int lastStart = -1000000000;
+
+    for (auto start : starts) {
+      if (lastStart + read->size() > start) {
+	continue;
+      }
+      lastStart=start;
+      
+      while (b < positions.size() &&
+	     positions[b].first < start) { ++b; }
+      while (e < positions.size() &&
+	     positions[e].first < start+windowSize) { ++e; }
+      assert(b <= e);
+
+      int score = 0;
+      if (FLAGS_long_read_algorithm == "lis") {
+        vector<pair<int, int> > pripremaZaLis;
+	for (int i = b; i < e; ++i) {
+	  pripremaZaLis.push_back(positions[i]);
+	}
+
+	vector<int> lisResult;
+        calcLongestIncreasingSubsequence(&lisResult, pripremaZaLis);
+	score = lisResult.size();
+      } else if (FLAGS_long_read_algorithm == "coverage") {
+	vector<Interval> intervals;
+	for (int i = b; i < e; ++i) {
+	  int a = positions[i].first;
+	  int b = positions[i].first + seedLen - 1;
+	  int c = positions[i].second;
+	  intervals.push_back(Interval(a,b,c));
+	}
+	cover(NULL, &score, intervals);
+      }
+
+#ifdef DEBUG
+      string geneSegment = cstrToString(genes[geneIdx]->data() + begin,
+					read->size());
+#else
+      string geneSegment = "";
+#endif
+
+      read->updateMapping(score, start, rc, geneIdx,
+			  genes[geneIdx]->description(), geneSegment);
+
+    }
   }
 }
 
@@ -248,7 +347,8 @@ void performMappingLong(vector<shared_ptr<Gene> >& genes,
 	printUsageAndExit();
       }
     } else { // --multiple_hits
-      //windowedAlignment();
+      // TODO: ovo je trenutno potrgano, ako stignem popravljam
+      // windowedAlignment(read, positionsByGene, idx, genes, rc);
     }
   }
 }
